42a71e4321c695e6fec3ee9380733b535cf0ce91
zaijianquirante7-cmd <zaijianquirante7@gmail.com>
2026-05-16 02:32:34 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,19 @@
-onload = () => {
+window.onload = () => {
     document.body.classList.remove("container");
-  };
 
-  const wrapper = document.querySelector(".wrapper");
-  const openBtn = document.getElementById("openBtn");
-  const closeBtn = document.getElementById("closeBtn");
-  
-  openBtn.addEventListener("click", () => {
-      wrapper.classList.add("open");
-      openBtn.style.display = "none";
-      closeBtn.style.display = "inline-block";
-  });
-  
-  closeBtn.addEventListener("click", () => {
-      wrapper.classList.remove("open");
-      closeBtn.style.display = "none";
-      openBtn.style.display = "inline-block";
-  });
-  
+    const wrapper = document.querySelector(".wrapper");
+    const openBtn = document.getElementById("openBtn");
+    const closeBtn = document.getElementById("closeBtn");
+
+    openBtn.addEventListener("click", () => {
+        wrapper.classList.add("open");
+        openBtn.style.display = "none";
+        closeBtn.style.display = "inline-block";
+    });
+
+    closeBtn.addEventListener("click", () => {
+        wrapper.classList.remove("open");
+        closeBtn.style.display = "none";
+        openBtn.style.display = "inline-block";
+    });
+};
